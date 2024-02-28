c7ac91631ed6b1d3ce20b81db9d96412127c9dfc
mohamad.basyir <basyirswift45@gmail.com>
2024-02-28 21:44:18 +0800

    started working on infinite scroll animation

--- src/main/java/com/inmyshoes/controller/HealthController.java
+++ src/main/java/com/inmyshoes/controller/HealthController.java
@@ -1,16 +1,22 @@
 package com.inmyshoes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import com.inmyshoes.service.CircumstanceService;
 
 @RestController
 public class HealthController {
     
+    @Autowired
+    CircumstanceService circumstanceService;
+
     @GetMapping("/")
     public ModelAndView ping() {
       
         ModelAndView mav = new ModelAndView();
+        mav.addObject("circumstances", circumstanceService.getAll());
         mav.setViewName("index");
         return mav;
 
